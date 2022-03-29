b8028c20b065b6bc581e7b98119c2343d08a4d5c
John Woolbright <jwoolbright23@gmail.com>
2022-03-29 10:34:33 -0500

    main.py cleaning up

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 from faker import Faker
 from random import randint
 
-# Writes a new file with first_name, last_name, email, and company
+# Todo, Tests and mutations
 
 def generate_random_user_data(faker_object, filename="example-user-data.csv", num_of_records=1000):
+    """Write a new file with first_name, last_name, email, and company.
+
+    Keyword arguments:
+    faker_object
+    filename -- name of file you are writing to or overwriting
+    """
     with open(filename, 'w') as the_file:
         for i in range(num_of_records):
             the_file.write("{},{},{},{}\n".format(fake.first_name(), fake.last_name(), fake.email(), fake.company()))
 
-# Writes a new file with timestamps, method, path, ip address
-
 def generate_random_ip_data(faker_object, filename="example-ip-data.csv", num_of_records=1000):
+    """Writes a new file with ipv4 address, user_agent, and past datetime.
+
+    Keyword arguments:
+    faker_object
+    filename -- name of file you are writing to or overwriting
+    """
     with open(filename, 'w') as the_file:
         for i in range(num_of_records):
             the_file.write("{},{},{}\n".format(fake.ipv4(), fake.user_agent(), fake.past_datetime()))
 
-# Writes a new file with username, password, and email
-
 def generate_sensitive_user_data(faker_object, filename="example-sensitive-user-data.csv", num_of_records=1000):
+    """Writes a new file with a user_name, password, and email.
+
+    Keyword arguments:
+    faker_object
+    filename -- name of file you are writing to or overwriting
+    """
     with open(filename, 'w') as the_file:
         for i in range(num_of_records):
             the_file.write("{},{},{}\n".format(fake.user_name, fake.password(), fake.email()))
 
-# Writes a new file with bank account #, account holder, money held, bank name
-
 def generate_random_bank_account_data(faker_object, filename="example-bank-account-data.csv", num_of_records=1000):
+    """Writes a new file with a name, company, city, and random integer.
+
+    Keyword arguments:
+    faker_object
+    filename -- name of file you are writing to or overwriting
+    """
     with open(filename, 'w') as the_file:
         for i in range(num_of_records):
             the_file.write("{},{},{},{}\n".format(fake.name(), fake.company(), fake.city(), randint(10000000, 999999999)))
