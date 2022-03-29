cee01572c310bd007e63f952073d3bde0b676b58
John Woolbright <jwoolbright23@gmail.com>
2022-03-29 15:41:26 -0500

    updated headers again

--- generate_random.py
+++ generate_random.py
@@ -14,7 +14,7 @@ def user_data(filename="example-user-data.csv", num_of_records=1000):
     with open(filename, 'w') as the_file:
         the_file.write("first_name,last_name,email,employer\n")
         for i in range(num_of_records):
-            company_choices = [fake.company(), "Boeing","VMLY&R","Express Scripts","Edward Jones","Centene","World Wide Technology", "Microsoft", "Ad Astra", "Commerce Bank", "mastercard", "spectrum", "Hunter Engineering", "Emerson Electric", "Freedom pay", "Washington University", "Nueroflow", "Accenture"]
+            company_choices = [fake.company().replace(",", ";"), "Boeing","VMLY&R","Express Scripts","Edward Jones","Centene","World Wide Technology", "Microsoft", "Ad Astra", "Commerce Bank", "mastercard", "spectrum", "Hunter Engineering", "Emerson Electric", "Freedom pay", "Washington University", "Nueroflow", "Accenture"]
             company_choices_weights = [2,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]
             company_list = random.choices(company_choices, company_choices_weights)
             company = company_list[0]
@@ -72,4 +72,4 @@ def transaction_data(filename="example-bank-account-data.csv", num_of_records=10
             multiplication_weights = [15, 12, 8, 6, 3, 2, 1]
             multiplication_amount = random.choices(multiplication_amounts, multiplication_weights)[0]
             bank_account_amount = random.random() * multiplication_amount
-            the_file.write("{},{},{},${:.2f}\n".format(fake.name(), fake.company(), fake.city(), bank_account_amount))
+            the_file.write("{},{},{},${:.2f}\n".format(fake.name(), fake.company().replace(",", ";"), fake.city(), bank_account_amount))
